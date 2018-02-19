81604042067f5a6edf16c88c47c2667a8ebfff18
Guilherme <gui.mochila@gmail.com>
2018-02-19 15:50:26 +0000

    Tracalorie: App.js started. ItemCtrl and App started.

---
+++ tracalorie-app/app.js
@@ -0,0 +1,52 @@
+// Storage Controller
+
+// Item Controller
+const ItemCtrl = (function() {
+  // Item Constructor
+  const Item = function(id, name, calories) {
+		this.id = id;
+		this.name = name;
+		this.calories = calories;
+	};
+
+	// Data Structure / State
+	const data = {
+		item: [
+			{ id: 0, name: 'Steak Dinner', calories: 1200 },
+			{ id: 1, name: 'Cookies', calories: 400 },
+			{ id: 2, name: 'Eggs', calories: 300 },
+		],
+		currentItem: null,
+		totalCalories: 0,
+	};
+
+	// Public Methods
+	return {
+		logData: function() {
+			return data;
+		}
+	}
+})();
+
+// UI controller
+const UICtrl = (function() {
+
+	// Public Methods
+	return {
+
+	}
+})();
+
+// App Controller
+const App = (function(ItemCtrl, UICtrl) {
+
+	// Public Methods
+	return {
+		init: function() {
+			console.log('Initializing App...');
+		}
+	}
+
+})(ItemCtrl, UICtrl);
+
+App.init();
